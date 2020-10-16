8997adf198a359e0b188cbfff1a7783223488963
Anik1998 <53035433+Anik1998@users.noreply.github.com>
2020-10-16 10:24:13 +0530

    Update Product.java

--- OnlineShopping/src/com/lti/model/Product.java
+++ OnlineShopping/src/com/lti/model/Product.java
@@ -2,10 +2,18 @@ package com.lti.model;
 
 public class Product {
 int productId;
+	int price;
 	String productName;
 	public int getProductId() {
 		return productId;
 	}
+	public int getProductId() {
+		return productId;
+	}
+	public int getprice() {
+		return price;
+	}
+	
 	public void setProductId(int productId) {
 		this.productId = productId;
 	}
